507aa8c1c4045bbaf89c5a9eaa5ff42ac2e8be27
Universebenzene <universebenzene@sina.com>
2019-02-14 21:34:11 +0800

    Update awesomerc and archmenu for office

--- archnaoc-awesome/.config/awesome/archmenu.lua
+++ archnaoc-awesome/.config/awesome/archmenu.lua
@@ -1,14 +1,16 @@
  local menu98edb85b00d9527ad5acebe451b3fae6 = {
      {"Application Finder", "xfce4-appfinder", "/usr/share/icons/gnome/16x16/actions/edit-find.png" },
      {"Archive Manager", "file-roller ", "/usr/share/icons/hicolor/16x16/apps/file-roller.png" },
-     {"Calculator", "gnome-calculator", "/usr/share/icons/gnome/16x16/apps/accessories-calculator.png" },
-     {"Catfish File Search", "/usr/bin/catfish", "/usr/share/pixmaps/catfish.svg" },
+     {"Calculator", "gnome-calculator", "/usr/share/icons/hicolor/16x16/apps/gnome-calculator.png" },
+     {"Catfish File Search", "/usr/bin/catfish ", "/usr/share/pixmaps/catfish.svg" },
      {"Disks", "gnome-disks", "/usr/share/icons/hicolor/16x16/apps/gnome-disks.png" },
      {"Mousepad", "mousepad ", "/usr/share/icons/gnome/16x16/apps/accessories-text-editor.png" },
      {"Screenshot", "gnome-screenshot --interactive", "/usr/share/icons/hicolor/48x48/apps/applets-screenshooter.png" },
      {"Screenshot", "xfce4-screenshooter", "/usr/share/icons/hicolor/48x48/apps/applets-screenshooter.png" },
-     {"compton", "compton --xrender-sync-fence", "/usr/share/icons/hicolor/48x48/apps/compton.png" },
+     {"Vim", "xterm -e vim ", "/usr/share/icons/hicolor/48x48/apps/gvim.png" },
+     {"compton", "compton", "/usr/share/icons/hicolor/48x48/apps/compton.png" },
      {"ipython", "xterm -e ipython", "/usr/share/pixmaps/ipython.png" },
+     {"ipython2", "xterm -e ipython2", "/usr/share/pixmaps/ipython2.png" },
  }
 
  local menude7a22a0c94aa64ba2449e520aa20c99 = {
@@ -17,10 +19,13 @@
  }
 
  local menud334dfcea59127bedfcdbe0a3ee7f494 = {
+     {"Flameshot", "flameshot", "/usr/share/icons/hicolor/48x48/apps/flameshot.png" },
+     {"KolourPaint", "kolourpaint ", "/usr/share/icons/hicolor/16x16/apps/kolourpaint.png" },
      {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
      {"Okular", "okular ", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
      {"Pinta", "pinta ", "/usr/share/icons/hicolor/16x16/apps/pinta.png" },
      {"Ristretto Image Viewer", "ristretto ", "/usr/share/icons/hicolor/16x16/apps/ristretto.png" },
+     {"XDvi", "xdvi "},
  }
 
  local menuc8205c7636e728d448c2774e6a4a944b = {
@@ -28,6 +33,7 @@
      {"Avahi VNC Server Browser", "/usr/bin/bvnc", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
      {"Electronic WeChat", "/usr/bin/electronic-wechat", "/usr/share/icons/hicolor/16x16/apps/electronic-wechat.png" },
      {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+     {"TeamViewer 14", "/opt/teamviewer/tv_bin/script/teamviewer", "/usr/share/icons/hicolor/16x16/apps/TeamViewer.png" },
      {"Thunderbird", "/usr/lib/thunderbird/thunderbird ", "/usr/share/icons/hicolor/16x16/apps/thunderbird.png" },
  }
 
@@ -45,6 +51,7 @@
 
  local menu6311ae17c1ee52b36e68aaf4ad066387 = {
      {" Topcat", "topcat", "/usr/share/pixmaps/topcat.png" },
+     {"Aladin", "aladin", "/usr/share/pixmaps/aladin.png" },
  }
 
  local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
@@ -59,24 +66,27 @@
      {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
      {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
      {"ipython", "xterm -e ipython", "/usr/share/pixmaps/ipython.png" },
+     {"ipython2", "xterm -e ipython2", "/usr/share/pixmaps/ipython2.png" },
  }
 
  local menu80d754c83abbbd4ef5d150904efcc86c = {
-     {"ds9", "ds9", "/usr/share/pixmaps/ds9.png" },
+     {"ds9", "ds9 ", "/usr/share/pixmaps/ds9.png" },
  }
 
  local menu52dd1c847264a75f400961bfb4d1c849 = {
      {"Audacious", "audacious ", "/usr/share/icons/hicolor/48x48/apps/audacious.png" },
      {"PulseAudio Volume Control", "pavucontrol", "/usr/share/icons/gnome/16x16/apps/multimedia-volume-control.png" },
      {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"Qt V4L2 video capture utility", "qvidcap", "/usr/share/icons/hicolor/16x16/apps/qvidcap.png" },
  }
 
  local menuee69799670a33f75d45c57d1d1cd0ab3 = {
      {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
      {"Bulk Rename", "/usr/lib/Thunar/ThunarBulkRename ", "/usr/share/icons/hicolor/16x16/apps/Thunar.png" },
      {"Fcitx", "fcitx", "/usr/share/icons/hicolor/16x16/apps/fcitx.png" },
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
      {"Manage Printing", "/usr/bin/xdg-open http://localhost:631/", "/usr/share/icons/hicolor/16x16/apps/cups.png" },
-     {"System Monitor", "gnome-system-monitor", "/usr/share/icons/gnome/16x16/apps/utilities-system-monitor.png" },
+     {"System Monitor", "gnome-system-monitor", "/usr/share/icons/hicolor/16x16/apps/gnome-system-monitor.png" },
      {"Thunar File Manager", "thunar ", "/usr/share/icons/hicolor/16x16/apps/Thunar.png" },
      {"Xfce Terminal", "xfce4-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
  }

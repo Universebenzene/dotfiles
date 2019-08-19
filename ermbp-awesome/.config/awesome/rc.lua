a10e7774096c7b71c43c9dee4975e4c391fb4551
Universebenzene <universebenzene@sina.com>
2019-08-19 23:07:36 +0800

    Correct for old awesomerc

--- ermbp-awesome/.config/awesome/rc.lua
+++ ermbp-awesome/.config/awesome/rc.lua
@@ -152,7 +152,7 @@ myawesomemenu = {
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
                                     { "open terminal", terminal },
-                                    { "Applications", xdgmenu} -- Benz
+                                    { "Applications", xdgmenu } -- Benz
                                   }
                         })
 
@@ -502,15 +502,33 @@ local taglist_buttons = gears.table.join(
                 )
 
 local tasklist_buttons = gears.table.join(
+-- -- -- -- -- Awesome 4.3 -- -- -- -- --
+--                      awful.button({ }, 1, function (c)
+--                                               if c == client.focus then
+--                                                   c.minimized = true
+--                                               else
+--                                                   c:emit_signal(
+--                                                       "request::activate",
+--                                                       "tasklist",
+--                                                       {raise = true}
+--                                                   )
+--                                               end
+--                                           end),
+-- -- -- -- -- -- -- -- -- -- -- -- -- --
                      awful.button({ }, 1, function (c)
                                               if c == client.focus then
                                                   c.minimized = true
                                               else
-                                                  c:emit_signal(
-                                                      "request::activate",
-                                                      "tasklist",
-                                                      {raise = true}
-                                                  )
+                                                  -- Without this, the following
+                                                  -- :isvisible() makes no sense
+                                                  c.minimized = false
+                                                  if not c:isvisible() and c.first_tag then
+                                                      c.first_tag:view_only()
+                                                  end
+                                                  -- This will also un-minimize
+                                                  -- the client, if needed
+                                                  client.focus = c
+                                                  c:raise()
                                               end
                                           end),
                      awful.button({ }, 3, function()

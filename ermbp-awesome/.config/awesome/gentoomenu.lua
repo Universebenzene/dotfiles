cb1690b9850ca8e4657bc522ab66dc78738e25aa
Universebenzene <universebenzene@sina.com>
2020-05-29 15:07:07 +0800

    edit gentoo awesome menu

--- ermbp-awesome/.config/awesome/gentoomenu.lua
+++ ermbp-awesome/.config/awesome/gentoomenu.lua
@@ -2,17 +2,17 @@
      {"Google Chrome", "/usr/bin/google-chrome-stable ", "/usr/share/icons/hicolor/16x16/apps/google-chrome.png" },
      {"Mozilla Firefox", "firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
      {"Steam", "/usr/bin/steam ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
-     {"TeamViewer 14", "teamviewer", "/usr/share/icons/hicolor/16x16/apps/TeamViewer.png" },
+     {"TeamViewer 0", "teamviewer", "/usr/share/icons/hicolor/16x16/apps/TeamViewer.png" },
      {"Telegram Desktop", "telegram-desktop -- ", "/usr/share/icons/hicolor/16x16/apps/telegram.png" },
-     {"Telegram Desktop", "/usr/lib64/telegram-desktop-bin/Telegram -- ", "/usr/share/icons/hicolor/16x16/apps/telegram.png" },
-     {"WPA Supplicant Administration GUI", "wpa_gui", "/usr/share/pixmaps/wpa_gui.svg" },
-     {"Web", "epiphany ", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Epiphany.png" },
+     {"Web", "epiphany ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Epiphany.svg" },
+     {"WeeChat", "xfce4-terminal -e weechat ", "/usr/share/icons/hicolor/16x16/apps/weechat.png" },
      {"baidunetdisk", "/usr/bin/baidunetdisk ", "/usr/share/icons/hicolor/16x16/apps/baidunetdisk.png" },
+     {"wpa_gui", "wpa_gui", "/usr/share/pixmaps/wpa_gui.svg" },
      {"远程桌面查看器", "vinagre "},
  }
 
  local menudb53804b7d726967c58fcc8c9ca03d27 = {
-     {"Evolution", "evolution ", "/usr/share/icons/hicolor/16x16/apps/evolution.png" },
+     {"Evolution", "evolution ", "/usr/share/icons/hicolor/16x16/apps/evolution-mail.png" },
      {"GoldenDict", "goldendict", "/usr/share/pixmaps/goldendict.png" },
      {"LibreOffice", "libreoffice ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-startcenter.png" },
      {"LibreOffice Base", "libreoffice --base ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-base.png" },
@@ -22,15 +22,16 @@
      {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
      {"LibreOffice Writer", "libreoffice --writer ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-writer.png" },
      {"Okular", "okular ", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
-     {"WPS 文字", "/usr/bin/wps ", "/usr/share/icons/hicolor/16x16/mimetypes/wps-office2019-wpsmain.png" },
-     {"WPS 演示", "/usr/bin/wpp ", "/usr/share/icons/hicolor/16x16/mimetypes/wps-office2019-wppmain.png" },
-     {"WPS 表格", "/usr/bin/et ", "/usr/share/icons/hicolor/16x16/mimetypes/wps-office2019-etmain.png" },
-     {"文档查看器", "evince ", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Evince.png" },
-     {"金山 PDF", "/usr/bin/wpspdf ", "/usr/share/icons/hicolor/16x16/mimetypes/wps-office2019-pdfmain.png" },
+     {"WPS 2019", "/usr/bin/wps ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-kprometheus.png" },
+     {"WPS PDF", "/usr/bin/wpspdf ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-pdfmain.png" },
+     {"WPS 文字", "/usr/bin/wps ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-wpsmain.png" },
+     {"WPS 演示", "/usr/bin/wpp ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-wppmain.png" },
+     {"WPS 表格", "/usr/bin/et ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-etmain.png" },
+     {"文档查看器", "evince ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Evince.svg" },
  }
 
  local menue1b7cee794666669f55acead93513971 = {
-     {"FontForge", "fontforge ", "/usr/share/icons/hicolor/16x16/apps/fontforge.png" },
+     {"FontForge", "fontforge ", "/usr/share/icons/hicolor/16x16/apps/org.fontforge.FontForge.png" },
      {"GNU Paint", "gpaint-2", "/usr/share/pixmaps/gpaint-2.xpm" },
      {"GNU 图像处理程序", "gimp-2.10 ", "/usr/share/icons/hicolor/16x16/apps/gimp.png" },
      {"KolourPaint", "kolourpaint ", "/usr/share/icons/hicolor/16x16/apps/kolourpaint.png" },
@@ -38,17 +39,19 @@
      {"MuPDF", "mupdf ", "/usr/share/pixmaps/mupdf.xpm" },
      {"Okular", "okular ", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
      {"XDVI", "xdvi", "/usr/share/pixmaps/xdvik.xpm" },
-     {"图像查看器", "eog ", "/usr/share/icons/hicolor/16x16/apps/eog.png" },
-     {"文档查看器", "evince ", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Evince.png" },
+     {"图像查看器", "eog ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.eog.svg" },
+     {"文档查看器", "evince ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Evince.svg" },
  }
 
  local menu526fe18cc54d217db3fdb0bf6c5e9bc8 = {
      {"Audacious", "audacious ", "/usr/share/icons/hicolor/48x48/apps/audacious.png" },
      {"GNOME MPlayer", "gnome-mplayer ", "/usr/share/icons/hicolor/16x16/apps/gnome-mplayer.png" },
+     {"Phonon Audio and Video", "phononsettings"},
      {"VLC 媒体播放器", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
  }
 
  local menu8640cb1581c2ae2de5a1462405b2943b = {
+     {" Topcat", "topcat", "/usr/share/icons/hicolor/72x72/apps/topcat.png" },
      {"GoldenDict", "goldendict", "/usr/share/pixmaps/goldendict.png" },
      {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
      {"SAOImage DS9", "ds9", "/usr/share/pixmaps/ds9.png" },
@@ -57,43 +60,41 @@
 
  local menuba08216f13dd1742157412386eee1225 = {
      {"KMines", "kmines -qwindowtitle KMines", "/usr/share/icons/hicolor/16x16/apps/kmines.png" },
-     {"Plague Inc: Evolved", "steam steam://rungameid/246620", "/home/benzene/.local/share/icons/hicolor/48x48/apps/steam_icon_246620.png"},
+     {"Plague Inc: Evolved", "steam steam://rungameid/246620", "/home/benzene/.local/share/icons/hicolor/48x48/apps/steam_icon_246620.png" },
      {"Steam", "/usr/bin/steam ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
  }
 
  local menude71492af019157e5a5ef958b570e760 = {
      {"Fcitx", "fcitx", "/usr/share/icons/hicolor/16x16/apps/fcitx.png" },
+     {"GParted", "/usr/bin/gparted ", "/usr/share/icons/hicolor/16x16/apps/gparted.png" },
      {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
      {"Oracle xVM VirtualBox", "VirtualBox", "/usr/share/icons/hicolor/16x16/apps/virtualbox.png" },
      {"Xfce 终端", "xfce4-terminal"},
      {"打印机管理", "/usr/bin/xdg-open http://localhost:631/", "/usr/share/icons/hicolor/16x16/apps/cups.png" },
-     {"系统监视器", "gnome-system-monitor", "/usr/share/icons/hicolor/16x16/apps/gnome-system-monitor.png" },
+     {"系统监视器", "gnome-system-monitor", "/usr/share/icons/hicolor/scalable/apps/org.gnome.SystemMonitor.svg" },
      {"终端", "gnome-terminal"},
      {"配置查看器", "gcm-viewer", "/usr/share/icons/hicolor/16x16/apps/gnome-color-manager.png" },
  }
 
  local menu41282bf95e43c64d579757573a03cdde = {
-     {"PyAlaMode (3.0)", "pyalamode-3.0", "/usr/share/pixmaps/PyCrust-3.0.png" },
-     {"PyCrust (3.0)", "pycrust-3.0", "/usr/share/pixmaps/PyCrust-3.0.png" },
-     {"PyShell (3.0)", "pyshell-3.0", "/usr/share/pixmaps/PyCrust-3.0.png" },
      {"Qt 5 Designer", "/usr/lib64/qt5/bin/designer", "/usr/share/icons/hicolor/128x128/apps/designer.png" },
      {"SDK-Maintenance-Tool", "/home/benzene/SailfishOS/SDKMaintenanceTool"},
      {"Sailfish OS IDE", "/home/benzene/SailfishOS/bin/qtcreator"},
-     {"XRCed (3.0)", "xrced-3.0", "/usr/share/pixmaps/XRCed-3.0.png" },
      {"sandbox", "xterm -e sandbox", "/usr/share/pixmaps/sandbox.svg" },
  }
 
  local menuc9a6ee90f5d43732e3f6cf4dcaa8493c = {
-     {"Fonts", "gnome-font-viewer "},
-     {"Vim", "xterm -e vim "},
-     {"compton", "compton"},
-     {"便笺", "bijiben ", "/usr/share/icons/hicolor/16x16/apps/org.gnome.bijiben.png" },
-     {"归档管理器", "file-roller ", "/usr/share/icons/hicolor/16x16/apps/file-roller.png" },
-     {"截图", "gnome-screenshot --interactive"},
-     {"文件", "nautilus --new-window ", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Nautilus.png" },
-     {"文本编辑器", "gedit ", "/usr/share/icons/hicolor/16x16/apps/gedit.png" },
-     {"磁盘", "gnome-disks", "/usr/share/icons/hicolor/16x16/apps/gnome-disks.png" },
-     {"计算器", "gnome-calculator", "/usr/share/icons/hicolor/16x16/apps/gnome-calculator.png" },
+     {"Fonts", "gnome-font-viewer ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.font-viewer.svg" },
+     {"Vim", "xterm -e vim ", "/usr/share/icons/hicolor/scalable/apps/gvim.svg"},
+     {"compton", "compton", "/usr/share/icons/hicolor/48x48/apps/compton.png" },
+     {"picom", "picom"},
+     {"便笺", "bijiben ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Notes.svg" },
+     {"归档管理器", "file-roller ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.ArchiveManager.svg" },
+     {"截图", "gnome-screenshot --interactive", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Screenshot.svg" },
+     {"文件", "nautilus --new-window ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Nautilus.svg" },
+     {"文本编辑器", "gedit ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.gedit.svg" },
+     {"磁盘", "gnome-disks", "/usr/share/icons/hicolor/scalable/apps/org.gnome.DiskUtility.svg" },
+     {"计算器", "gnome-calculator", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Calculator.svg" },
  }
 
 xdgmenu = {

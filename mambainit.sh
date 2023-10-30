5de4efd78e8da438ac6d6994bef5fd5bd50faf05
Universebenzene <universebenzene@sina.com>
2023-10-30 12:22:47 +0800

    Change to relative homedir in mambainit

--- mambainit.sh
+++ mambainit.sh
@@ -2,7 +2,7 @@
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
 export MAMBA_EXE='/usr/bin/micromamba';
-export MAMBA_ROOT_PREFIX='/home/gsadmin/.micromamba';
+export MAMBA_ROOT_PREFIX="~/.micromamba";
 __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"

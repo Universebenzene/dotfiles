9ad304499d08029d35b540597513631f1ccb8ee4
Universebenzene <universebenzene@sina.com>
2025-05-14 21:46:25 +0800

    Change micromamba to mamba

--- mambainit.sh
+++ mambainit.sh
@@ -1,7 +1,7 @@
 # micromamba shell init --shell zsh --root-prefix=~/.micromamba
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE='/usr/bin/micromamba';
+export MAMBA_EXE='/usr/bin/mamba';
 export MAMBA_ROOT_PREFIX="~/.micromamba";
 __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
